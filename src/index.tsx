4592ee920d579733185e6dd8741e81ed890c85f9
Jagrati Gupta <123jagratigupta@gmail.com>
2024-04-20 13:35:32 +0530

    JG: create routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Menu from "./components/Menu";
+import Orders from "./components/Orders";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
+      },
+      {
+        path: "/logout",
+        element: "Logout",
+      },
+    ],
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 reportWebVitals();
