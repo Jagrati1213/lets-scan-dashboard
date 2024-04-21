5b236bd203ddc4a2bc6e1dff325072b4c10bce40
Jagrati Gupta <123jagratigupta@gmail.com>
2024-04-22 01:33:34 +0530

    JG: create menu details drawer

---
+++ src/components/Menu/MenuViewDrawer.tsx
@@ -0,0 +1,38 @@
+import { Drawer, Flex, Image, Rate } from "antd";
+import React from "react";
+import { MenuListType } from "../../types";
+import Title from "antd/es/typography/Title";
+import Style from "../../styles/_MenuViewDrawer.module.scss";
+
+interface MenuProps {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  menuData: MenuListType;
+}
+export default function MenuViewDrawer({ open, setOpen, menuData }: MenuProps) {
+  const onClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className={Style.menu_view_drawer}>
+      <Drawer
+        title="Menu Details"
+        placement="right"
+        closable={true}
+        onClose={onClose}
+        open={open}
+        getContainer={false}
+      >
+        <Flex gap={"15px"} vertical align="center">
+          <Image src={menuData?.image} preview={false} />
+          <Flex vertical align="start" style={{ width: "100%" }}>
+            <Title level={5}>{menuData?.name}</Title>
+            <p>{menuData?.desc}</p>
+            <p className="price">{`Price : ${menuData?.price}`}</p>
+            <Rate disabled allowHalf defaultValue={2.5} />
+          </Flex>
+        </Flex>
+      </Drawer>
+    </div>
+  );
+}
