ed6c57d53e93b80953db4f72fc31c83ad10f5313
Jagrati Gupta <123jagratigupta@gmail.com>
2024-04-22 12:41:15 +0530

    JG: add drawer and layout

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,68 +1,90 @@
-import { Avatar, Button, Flex, Table } from "antd";
-import type { TableColumnsType } from "antd";
+import { Avatar, Button, Flex, List } from "antd";
 import Title from "antd/es/typography/Title";
+import { useState } from "react";
+import MenuViewDrawer from "./MenuViewDrawer";
+import AddMenuDrawer from "./AddMenuDrawer";
+import { MenuListType } from "../../types";
 import Style from "../../styles/_Menu.module.scss";
 
-interface DataType {
-  key: React.Key;
-  image: string;
-  name: string;
-  price: number;
-  desc: string;
-}
 export default function Menu() {
-  const columns: TableColumnsType<DataType> = [
+  // states
+  const [isAddMenuOpen, setIsAddMenuOpen] = useState<boolean>(false);
+  const [isMenuViewOpen, setIsMenuViewOpen] = useState<boolean>(false);
+  const [menuList, setMenuList] = useState<MenuListType[]>([
     {
-      title: "Image",
-      width: 100,
-      dataIndex: "image",
-      key: "image",
-      fixed: "left",
-      render: (image) => <Avatar src={image} />,
+      id: 1,
+      name: "Menu 1",
+      image:
+        "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png",
+      price: 200,
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero quos sit modi aliquid aperiam aut at et rem tempora, voluptate porro, laudantium magnam quam? Doloribus autem, adipisci placeat reprehenderit est provident, repudiandae aliquam dicta atque illo nesciunt, corporis repellat fugit maxime? Eaque temporibus",
     },
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      id: 2,
+      name: "Menu 2",
+      image:
+        "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png",
+      price: 200,
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero quos sit modi aliquid aperiam aut at et rem tempora, voluptate porro, laudantium magnam quam? Doloribus autem, adipisci placeat reprehenderit est provident, repudiandae aliquam dicta atque illo nesciunt, corporis repellat fugit maxime? Eaque temporibus",
     },
-    {
-      title: "Price",
-      dataIndex: "price",
-      key: "price",
-    },
-    {
-      title: "Description",
-      dataIndex: "desc",
-      key: "desc",
-    },
-  ];
+  ]);
+  const [menuDetails, setMenuDetails] = useState<MenuListType>(menuList[0]);
 
-  const data: DataType[] = [];
-  for (let i = 0; i < 10; i++) {
-    data.push({
-      key: i,
-      image: `https://api.dicebear.com/7.x/miniavs/svg?seed=${i}`,
-      name: `Edward ${i}`,
-      price: i,
-      desc: `London Park no. ${i}`,
-    });
-  }
+  // methods
+  const showAddMenu = () => {
+    setIsAddMenuOpen(true);
+  };
+
+  const showMenuDetails = () => {
+    setIsMenuViewOpen(true);
+  };
 
   return (
     <div className={Style.menu_container}>
-      <Flex gap="middle" vertical>
+      <Flex gap="40px" vertical>
         <Flex justify={"space-between"} align="center">
           <Title level={5}>Menus </Title>
-          <Button type="primary" shape="round">
+          <Button type="primary" shape="round" onClick={showAddMenu}>
             Add Menu
           </Button>
         </Flex>
-        <Table
-          className="menu_list"
-          columns={columns}
-          dataSource={data}
-          scroll={{ x: 1500, y: 600 }}
+
+        <List
+          dataSource={menuList}
+          bordered
+          renderItem={(item, index) => (
+            <List.Item
+              key={item.id}
+              actions={[
+                <p>&#8377; {item?.price}</p>,
+                <Button
+                  type="link"
+                  onClick={() => {
+                    setMenuDetails(menuList[index]);
+                    showMenuDetails();
+                  }}
+                >
+                  View
+                </Button>,
+                <Button type="link" onClick={showMenuDetails}>
+                  Edit
+                </Button>,
+              ]}
+            >
+              <List.Item.Meta
+                avatar={<Avatar shape="square" size="large" src={item.image} />}
+                title={<a href="https://ant.design/index-cn">{item.name}</a>}
+                description={`${item.desc?.slice(0, 60)}...`}
+              />
+            </List.Item>
+          )}
         />
+        <MenuViewDrawer
+          open={isMenuViewOpen}
+          setOpen={setIsMenuViewOpen}
+          menuData={menuDetails}
+        />
+        <AddMenuDrawer open={isAddMenuOpen} setOpen={setIsAddMenuOpen} />
       </Flex>
     </div>
   );
