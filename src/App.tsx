584b3cedba334566e9387664d1b661687e367890
Jagrati Gupta <123jagratigupta@gmail.com>
2024-04-25 21:44:40 +0530

    JG: add authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { MainContainer } from "./components/MainContainer";
 import "./styles/global.scss";
 import "./App.scss";
+import Authentication from "./components/Authentication";
+import { useAppSelector } from "./store/store";
 
 function App() {
+  const auth = useAppSelector((store) => store.auth);
+  console.log(auth);
   return (
     <div className="App">
-      <MainContainer />
+      {auth.userName && auth.password ? <MainContainer /> : <Authentication />}
     </div>
   );
 }
