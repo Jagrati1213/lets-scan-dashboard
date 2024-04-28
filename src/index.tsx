f1d6f9fa460d976dd8852b419c4bf63c9ca52056
Jagrati Gupta <123jagratigupta@gmail.com>
2024-04-28 18:42:29 +0530

    JG: remove

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Menu from "./components/Menu";
 import Orders from "./components/Orders";
@@ -39,4 +38,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-reportWebVitals();
