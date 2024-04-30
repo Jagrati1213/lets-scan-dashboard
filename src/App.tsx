b775ff81a43113143229ebe726e74f56468e39aa
Jagrati Gupta <123jagratigupta@gmail.com>
2024-04-30 17:14:39 +0530

    JG: add token & refresh token

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,35 @@ import "./styles/global.scss";
 import "./App.scss";
 import Authentication from "./components/Authentication";
 import { useAppSelector } from "./store/store";
+import { useEffect } from "react";
+import { getUserHandler, refreshTokenHandler } from "./utils/getUserHandler";
+import { useDispatch } from "react-redux";
+import { setUserDetails } from "./store/slices/userSlice";
 
 function App() {
-  const auth = useAppSelector((store) => store.auth);
-  console.log(auth);
+  const dispatch = useDispatch();
+  const { user } = useAppSelector((store) => store.auth);
+  let firstRender = true;
+
+  useEffect(() => {
+    if (firstRender) {
+      firstRender = false;
+      getUserHandler()
+        .then((data) => dispatch(setUserDetails(data)))
+        .catch((error) => console.log(error));
+    }
+
+    let interval = setTimeout(() => {
+      refreshTokenHandler()
+        .then((data) => dispatch(setUserDetails(data)))
+        .catch((error) => console.log(error));
+    }, 1000 * 30);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="App">
-      {auth.userName && auth.password ? <MainContainer /> : <Authentication />}
+      {user.email && user.email ? <MainContainer /> : <Authentication />}
     </div>
   );
 }
