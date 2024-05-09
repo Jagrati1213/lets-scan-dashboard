8f4d0f22fd41e065585eaa27317626c23a7926dc
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-10 01:31:30 +0530

    JG: create logout button

--- src/components/SiderContainer.tsx
+++ src/components/SiderContainer.tsx
@@ -1,4 +1,4 @@
-import { Menu, MenuProps } from "antd";
+import { Menu, MenuProps, message } from "antd";
 import Sider from "antd/es/layout/Sider";
 import { useState } from "react";
 import { IoHome } from "react-icons/io5";
@@ -7,32 +7,49 @@ import { FaBoxOpen } from "react-icons/fa6";
 import { RiLogoutCircleFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import Style from "../styles/_SiderContainer.module.scss";
-
-const items: MenuProps["items"] = [
-  {
-    label: <Link to="/">Dashboard</Link>,
-    key: "home",
-    icon: <IoHome />,
-  },
-  {
-    label: <Link to="/menu">Menu</Link>,
-    key: "menu",
-    icon: <MdRestaurantMenu />,
-  },
-  {
-    label: <Link to="/orders">Order List</Link>,
-    key: "order",
-    icon: <FaBoxOpen />,
-  },
-  {
-    label: <Link to="/logout">Log Out</Link>,
-    key: "logout",
-    icon: <RiLogoutCircleFill />,
-  },
-];
+import { apiClient } from "../global";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../store/slices/userSlice";
 
 export const SiderContainer = () => {
+  // COLLAPSE MENU SIDEBAR
   const [collapsed, setCollapsed] = useState(false);
+  const dispatch = useDispatch();
+
+  // HANDLE FOR LOG OUT
+  const handleLogout = async () => {
+    const response = await apiClient.get("logout");
+    const { statusText, success } = await response.data;
+    if (success) {
+      message.success(statusText);
+      dispatch(removeUser());
+    } else message.error(statusText);
+  };
+
+  // SIDE MENU LIST
+  const items: MenuProps["items"] = [
+    {
+      label: <Link to="/">Dashboard</Link>,
+      key: "home",
+      icon: <IoHome />,
+    },
+    {
+      label: <Link to="/menu">Menu</Link>,
+      key: "menu",
+      icon: <MdRestaurantMenu />,
+    },
+    {
+      label: <Link to="/orders">Order List</Link>,
+      key: "order",
+      icon: <FaBoxOpen />,
+    },
+    {
+      label: "Log Out",
+      key: "logout",
+      icon: <RiLogoutCircleFill />,
+      onClick: handleLogout,
+    },
+  ];
 
   return (
     <Sider
