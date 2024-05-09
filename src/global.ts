2d6933ab93e500a9409c1682e66eb7ad5756cfda
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-10 01:12:44 +0530

    JG: change base  url

--- src/global.ts
+++ src/global.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 // Api url for access the options
 export const apiClient = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://localhost:3000/api/v1/user/",
+  withCredentials: true,
 });
