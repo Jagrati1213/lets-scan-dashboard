58717b64b99f00aa99e31444a16d640952872a28
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-10 01:13:40 +0530

    JG: create get handler

--- src/utils/getUserHandler.ts
+++ src/utils/getUserHandler.ts
@@ -1,22 +1,11 @@
 import { apiClient } from "../global";
 
-apiClient.defaults.withCredentials = true;
-
-export const refreshTokenHandler = async () => {
+export const getUserDetails = async () => {
   try {
-    const response = await apiClient.get("refresh", { withCredentials: true });
-    const data = await response.data;
+    const response = await apiClient.post("user-details");
+    const { data } = await response.data;
     return data;
   } catch (error) {
-    console.log(error);
-  }
-};
-export const getUserHandler = async () => {
-  try {
-    const response = await apiClient.get("user", { withCredentials: true });
-    const data = await response.data;
-    return data;
-  } catch (error) {
-    console.log(error);
+    console.log("ERROR IN API OF GET DETAILS, ", error);
   }
 };
