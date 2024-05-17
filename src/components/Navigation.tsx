f313bd6c43bfc8d25ef8e4341c571a557f247e2a
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-17 17:17:10 +0530

    JG: add user details

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,23 @@
+import { Avatar, Flex } from "antd";
+import { useAppSelector } from "../store/store";
 import Style from "../styles/_Navigation.module.scss";
 
 export const Navigation = () => {
+  const { user } = useAppSelector((store) => store.auth);
+  console.log(user.username);
   return (
     <div className={Style.nav}>
-      <h1>OrderMinder</h1>
+      <Flex align="center" justify="space-between">
+        <h1>OrderMinder</h1>
+        {user.username && (
+          <Flex align="center" justify="space-between" gap={"10px"}>
+            <p>{user.username}</p>
+            <Avatar className={Style.user_avatar}>
+              {user.username.slice(0, 1).toUpperCase()}
+            </Avatar>
+          </Flex>
+        )}
+      </Flex>
     </div>
   );
 };
