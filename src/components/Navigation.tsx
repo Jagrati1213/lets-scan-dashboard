41efe76a8eaa1b028e78d8671f632696c9d5d768
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-17 19:26:16 +0530

    JG: change the element  && responsive

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,22 @@
-import { Avatar, Flex } from "antd";
+import { Avatar, Flex, Tooltip } from "antd";
 import { useAppSelector } from "../store/store";
 import Style from "../styles/_Navigation.module.scss";
+import { useEffect, useState } from "react";
 
 export const Navigation = () => {
   const { user } = useAppSelector((store) => store.auth);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={Style.nav}>
       <Flex align="center" justify="space-between">
@@ -11,9 +24,17 @@ export const Navigation = () => {
         {user.username && (
           <Flex align="center" justify="space-between" gap={"10px"}>
             <p>{user.username}</p>
-            <Avatar className={Style.user_avatar}>
-              {user.username.slice(0, 1).toUpperCase()}
-            </Avatar>
+            {windowWidth >= 575 ? (
+              <Avatar className={Style.user_avatar}>
+                {user.username.slice(0, 1).toUpperCase()}
+              </Avatar>
+            ) : (
+              <Tooltip title={user?.username}>
+                <Avatar className={Style.user_avatar}>
+                  {user.username.slice(0, 1).toUpperCase()}
+                </Avatar>
+              </Tooltip>
+            )}
           </Flex>
         )}
       </Flex>
