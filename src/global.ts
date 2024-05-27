e3bc412e0e9d84cd75b8f19cf4c5c9c9eb0ea040
Jagrati Gupta <123jagratigupta@gmail.com>
2024-05-27 14:11:49 +0530

    JG: change port

--- src/global.ts
+++ src/global.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // AXIOS BASE URL
 export const Axios = axios.create({
-  baseURL: "http://localhost:3000/api/v1/user/",
+  baseURL: "http://localhost:4000/api/v1/user/",
   withCredentials: true,
 });
 
