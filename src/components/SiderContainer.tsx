56c851eb5d6c71f1f40d0ddd11b1b6f9e741cc2a
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-06 00:56:11 +0530

    JG: change api path

--- src/components/SiderContainer.tsx
+++ src/components/SiderContainer.tsx
@@ -20,7 +20,7 @@ export const SiderContainer = () => {
 
   // HANDLE FOR LOG OUT
   const handleLogout = async () => {
-    const response = await Axios.get("logout");
+    const response = await Axios.get("user/logout");
     const { statusText, success } = await response.data;
     if (success) {
       message.success(statusText);
