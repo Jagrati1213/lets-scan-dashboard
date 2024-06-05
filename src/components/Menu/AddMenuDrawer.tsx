a4a1a1e4a106fea291866563c685c82cac5d90ea
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-06 00:55:31 +0530

    JG: add functionality for item creation

--- src/components/Menu/AddMenuDrawer.tsx
+++ src/components/Menu/AddMenuDrawer.tsx
@@ -1,69 +1,136 @@
-import { Button, Drawer, Form, FormProps, Input, InputNumber } from "antd";
-import React from "react";
+import {
+  Button,
+  Drawer,
+  Form,
+  FormProps,
+  Input,
+  InputNumber,
+  Spin,
+} from "antd";
+import React, { FormEvent, useState } from "react";
 import { MenuFormProps, drawerOptions } from "../../types";
 import Style from "../../styles/_AddMenuDrawer.module.scss";
+import { createMenuItemHandler } from "../../utils/menuItemHandler";
 interface AddMenuProps {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<drawerOptions>>;
 }
 
 export default function AddMenuDrawer({ open, setOpen }: AddMenuProps) {
+  const [file, setFile] = useState<File | undefined | any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [form] = Form.useForm();
+
   const onClose = () => {
     setOpen((prev) => ({ ...prev, isAddMenuOpen: false }));
   };
-  const onFinish: FormProps<MenuFormProps>["onFinish"] = (values) => {
-    console.log("Success:", values);
+
+  // UPLOAD FILE
+  const handleUploadImage = (event: FormEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement & {
+      files: FileList;
+    };
+    setFile(target?.files[0]);
   };
 
+  // SEND ITEM"S DATA TO API
+  const handleMenuItemCreate = async (values: any) => {
+    values.image = file;
+
+    // Append the form data to the FormData object
+    const data = new FormData();
+    data.append("name", values.name);
+    data.append("price", values.price.toString());
+    data.append("desc", values.desc);
+    if (values.image) {
+      data.append("image", values.image);
+    }
+    //INVOKE CREATE MENU API
+    setIsLoading(true);
+    await createMenuItemHandler(values);
+    setIsLoading(false);
+    form.resetFields();
+    onClose();
+  };
+
+  // IF USER NOT ADDED FIELD
   const onFinishFailed: FormProps<MenuFormProps>["onFinishFailed"] = (
     errorInfo
   ) => {
     console.log("Failed:", errorInfo);
   };
+
   return (
     <div className={Style.add_menu_drawer}>
       <Drawer
         title="Add Menu"
         placement="right"
-        closable={true}
+        closable={!isLoading}
         onClose={onClose}
         open={open}
         getContainer={false}
+        maskClosable={!isLoading}
       >
-        <Form
-          name="add-menu"
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-          layout="vertical"
-          variant="outlined"
-        >
-          <Form.Item<MenuFormProps>
-            label="Name"
-            name="name"
-            rules={[{ required: true, message: "add item name!" }]}
+        <Spin spinning={isLoading}>
+          <Form
+            name="add-menu"
+            form={form}
+            onFinish={handleMenuItemCreate}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            layout="vertical"
+            variant="outlined"
+            encType="multipart/form-data"
           >
-            <Input placeholder="dosh" />
-          </Form.Item>
+            <Form.Item<MenuFormProps>
+              label="Item Name"
+              name="name"
+              rules={[{ required: true, message: "add item name!" }]}
+            >
+              <Input placeholder="dosh" />
+            </Form.Item>
 
-          <Form.Item<MenuFormProps>
-            label="price"
-            name="price"
-            rules={[{ required: true, message: "add price" }]}
-          >
-            <InputNumber style={{ width: "100%" }} min={10} placeholder="100" />
-          </Form.Item>
+            <Form.Item<MenuFormProps>
+              label="Item Price"
+              name="price"
+              rules={[{ required: true, message: "add price" }]}
+            >
+              <InputNumber
+                style={{ width: "100%" }}
+                min={10}
+                placeholder="100"
+              />
+            </Form.Item>
 
-          <Form.Item<MenuFormProps> label="Description" name="desc">
-            <Input.TextArea placeholder="delicious food" rows={13} />
-          </Form.Item>
+            <Form.Item<MenuFormProps>
+              name={"image"}
+              rules={[{ required: true, message: "upload item image" }]}
+            >
+              <input
+                type="file"
+                name="image"
+                value={file}
+                onChange={handleUploadImage}
+                accept="image/*"
+                required
+              />
+            </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Add Menu
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item<MenuFormProps>
+              label="Description"
+              name="desc"
+              rules={[{ required: true, message: "Add item details" }]}
+            >
+              <Input.TextArea placeholder="delicious food" rows={13} />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" loading={isLoading}>
+                Add Menu
+              </Button>
+            </Form.Item>
+          </Form>
+        </Spin>
       </Drawer>
     </div>
   );
