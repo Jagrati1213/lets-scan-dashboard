e0eb1776f1db51bda6b5eaeb1d504d02d923e3bc
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-07 00:31:18 +0530

    JG: add to api & types

---
+++ src/apis/getUserHandler.ts
@@ -0,0 +1,13 @@
+import { Axios } from "../global";
+import { UserResponseType } from "../types";
+
+export const getUserDetails = async (): Promise<UserResponseType["data"]> => {
+  try {
+    const response = await Axios.get<UserResponseType>("user");
+    const { data } = response.data;
+    return data;
+  } catch (error: any) {
+    console.log("ERROR IN FETCH USER DETAILS", error);
+    return error;
+  }
+};
