584b8bf5bb1b76959950a8d46225be1a6a520d1e
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-11 02:23:03 +0530

    JG: view Item

--- src/components/Menu/MenuViewDrawer.tsx
+++ src/components/Menu/MenuViewDrawer.tsx
@@ -1,18 +1,36 @@
 import { Drawer, Flex, Image, Rate } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MenuListType, DrawerOptionsType } from "../../types";
 import Title from "antd/es/typography/Title";
 import Style from "../../styles/_MenuViewDrawer.module.scss";
+import { useAppSelector } from "../../store/store";
+import MenuItem from "antd/es/menu/MenuItem";
 
 interface MenuProps {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<DrawerOptionsType>>;
-  menuData: MenuListType;
+  menuItemId: string | undefined | null;
 }
-export default function MenuViewDrawer({ open, setOpen, menuData }: MenuProps) {
+export default function MenuViewDrawer({
+  open,
+  setOpen,
+  menuItemId,
+}: MenuProps) {
+  // STATE
+  const { menulist } = useAppSelector((store) => store.menuListSlice);
+  const [menuItem, setMenuItem] = useState<MenuListType>();
+
+  // CLOSE DRAWER
   const onClose = () => {
     setOpen((prev) => ({ ...prev, isMenuViewOpen: false }));
   };
+
+  // RENDER MENU ITEM
+  useEffect(() => {
+    const menuItem = menulist.find((item) => item._id === menuItemId);
+    setMenuItem(menuItem);
+  }, [menuItemId]);
+
   return (
     <div className={Style.menu_view_drawer}>
       <Drawer
@@ -24,11 +42,11 @@ export default function MenuViewDrawer({ open, setOpen, menuData }: MenuProps) {
         getContainer={false}
       >
         <Flex gap={"15px"} vertical align="center">
-          <Image src={menuData?.image} preview={false} />
+          <Image src={menuItem?.image} preview={false} />
           <Flex vertical align="start" style={{ width: "100%" }}>
-            <Title level={5}>{menuData?.name}</Title>
-            <p>{menuData?.description}</p>
-            <p className="price">{`Price : ${menuData?.price}`}</p>
+            <Title level={5}>{menuItem?.name}</Title>
+            <p>{menuItem?.description}</p>
+            <p className="price">{`Price : ${menuItem?.price}`}</p>
             <Rate disabled allowHalf defaultValue={2.5} />
           </Flex>
         </Flex>
