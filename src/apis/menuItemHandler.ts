917ab362d8bb3ee19e9813689c63d92e2141b158
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-11 02:17:35 +0530

    JG: add handler for updating

--- src/apis/menuItemHandler.ts
+++ src/apis/menuItemHandler.ts
@@ -1,7 +1,12 @@
 import { message } from "antd";
 import { Axios } from "../global";
-import { MenuListResponseType } from "../types";
+import {
+  MenuImageUploadedTypes,
+  MenuListResponseType,
+  MenuUpdateBodyProps,
+} from "../types";
 
+// CREATE MENU ITEM
 export const createMenuItemHandler = async (
   values: FormData
 ): Promise<MenuListResponseType["data"]> => {
@@ -23,6 +28,7 @@ export const createMenuItemHandler = async (
   }
 };
 
+// FETCH MENU LIST
 export const getMenuList = async (): Promise<MenuListResponseType["data"]> => {
   try {
     const response = await Axios.get("menu");
@@ -30,6 +36,48 @@ export const getMenuList = async (): Promise<MenuListResponseType["data"]> => {
     return data;
   } catch (error: any) {
     console.log("ERROR IN FETCH MENULIST,", error);
+    return error;
+  }
+};
+
+// UPDATE MENU ITEM
+export const updateMenuItemHandler = async (values: MenuUpdateBodyProps) => {
+  try {
+    const response = await Axios.post("menu/update-menu", values, {
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const { success, statusText, data } = response.data;
+    if (success) {
+      message.success(statusText);
+    } else {
+      message.error(statusText);
+    }
+    return data;
+  } catch (error: any) {
+    console.log("ERROR IN CREATE ITEM FOR MENU,", error);
+    return error;
+  }
+};
+
+// UPLOADED MENU IMAGE
+export const uploadMenuItemImage = async (
+  image: FormData
+): Promise<MenuImageUploadedTypes["data"]> => {
+  try {
+    const response = await Axios.post("menu/upload-image", image, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+
+    const { success, statusText, data } = response.data;
+    if (success) {
+      message.success(statusText);
+    } else {
+      message.error(statusText);
+    }
+    return data?.url;
+  } catch (error: any) {
+    console.log("ERROR IN CREATE ITEM FOR MENU,", error);
     return error;
   }
 };
