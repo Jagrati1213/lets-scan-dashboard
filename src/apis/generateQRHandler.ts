af14d8be321aba5e22818a90a57350576d2e59e4
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-14 17:54:13 +0530

    JG: add constant

--- src/apis/generateQRHandler.ts
+++ src/apis/generateQRHandler.ts
@@ -1,4 +1,6 @@
+import { menuMuseWebsitePath } from "../global";
+
 export const generateQRCodeHandler = async (id: string) => {
-  const QRimageSrc = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://menu-muse.vercel.app/menu/${id}`;
+  const QRimageSrc = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${menuMuseWebsitePath}/menu/${id}`;
   return { QRimageSrc, url: `https://menu-muse.vercel.app/menu/${id}` };
 };
