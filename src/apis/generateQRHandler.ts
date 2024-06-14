68bf0d3289449e63e5236425e4b93d3559714ba0
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-14 17:51:27 +0530

    JG: Return url as well

--- src/apis/generateQRHandler.ts
+++ src/apis/generateQRHandler.ts
@@ -1,5 +1,4 @@
 export const generateQRCodeHandler = async (id: string) => {
-  console.log("click");
   const QRimageSrc = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://menu-muse.vercel.app/menu/${id}`;
-  return QRimageSrc;
+  return { QRimageSrc, url: `https://menu-muse.vercel.app/menu/${id}` };
 };
