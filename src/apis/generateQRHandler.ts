181b440989b7179e37ee8fa1be949d8cd8db2397
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-15 02:00:29 +0530

    JG: remove

--- src/apis/generateQRHandler.ts
+++
@@ -1,6 +0,0 @@
-import { menuMuseWebsitePath } from "../global";
-
-export const generateQRCodeHandler = async (id: string) => {
-  const QRimageSrc = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${menuMuseWebsitePath}/menu/${id}`;
-  return { QRimageSrc, url: `https://menu-muse.vercel.app/menu/${id}` };
-};
--- src/components/QrGenerator/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function QrGenerator() {
-  return <div>QrGenerator</div>;
-}
