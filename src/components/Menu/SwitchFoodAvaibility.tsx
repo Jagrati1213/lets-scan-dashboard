c90e13a06032f5f1685ec0e1dd1b7c90239dc766
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-15 17:12:58 +0530

    JG: create switch component

---
+++ src/components/Menu/SwitchFoodAvaibility.tsx
@@ -0,0 +1,68 @@
+import { Popconfirm, PopconfirmProps, Switch, message } from "antd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/store";
+import { changeFoodType } from "../../apis/changeFoodTypeHandler";
+import { updateMenuItemAction } from "../../store/slices/menuListSlice";
+
+export default function SwitchFoodAvailability({
+  menuItemId,
+}: {
+  menuItemId: string | null;
+}) {
+  // STATES
+  const { menulist } = useAppSelector((store) => store.menuListSlice);
+  const dispatch = useAppDispatch();
+
+  const [active, setActive] = useState(false);
+
+  //   SET ACTIVE TRUE
+  const confirm: PopconfirmProps["onConfirm"] = async () => {
+    setActive(true);
+    const data = await changeFoodType({
+      activeVal: true,
+      menuId: menuItemId,
+    });
+    dispatch(updateMenuItemAction(data));
+  };
+
+  // CHANGE FOOD ACTIVE
+  const switchFoodActive = async (checked: boolean) => {
+    if (active) {
+      setActive(false);
+      const data = await changeFoodType({
+        activeVal: checked,
+        menuId: menuItemId,
+      });
+      dispatch(updateMenuItemAction(data));
+    }
+  };
+
+  useEffect(() => {
+    const menuItem = menulist.find((item) => item?._id === menuItemId);
+    if (menuItem) {
+      setActive(menuItem.isActive);
+    }
+  }, [menuItemId]);
+
+  return active ? (
+    <Switch defaultChecked size="small" onChange={switchFoodActive} />
+  ) : (
+    <Popconfirm
+      title="Switch the Food"
+      description="Are you sure to active this item?"
+      onConfirm={confirm}
+      okText="Yes"
+      cancelText="No"
+      icon={<QuestionCircleOutlined style={{ color: "red" }} />}
+    >
+      <Switch
+        defaultChecked
+        size="small"
+        onChange={switchFoodActive}
+        value={active}
+      />
+      ,
+    </Popconfirm>
+  );
+}
