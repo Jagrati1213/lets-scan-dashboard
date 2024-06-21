0fa418a4181490ff7e6ad892c204cc993a338f4f
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-21 17:26:29 +0530

    JG: order slice added

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
 import menuListSlice from "./slices/menuListSlice";
 import vendorSlice from "./slices/vendorSlice";
+import orderListSlice from "./slices/orderListSlice";
 
 export const store = configureStore({
   reducer: {
     authSlice: vendorSlice,
     menuListSlice: menuListSlice,
+    orderListSlice: orderListSlice,
   },
 });
 
