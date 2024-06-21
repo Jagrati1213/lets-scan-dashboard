d4e7064aff85b8f430ef26627134027ad4d1d251
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-21 17:24:56 +0530

    JG: Fetched orders details

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,3 +1,67 @@
+import { Col, Row, Tabs, TabsProps, Typography } from "antd";
+import Style from "../../styles/_Order.module.scss";
+import { PaymentOrderTable } from "./PaymentOrderTable";
+import { getOrdersApi } from "../../apis/order/getOrders";
+import { useSearchParams } from "react-router-dom";
+import { useCallback, useEffect } from "react";
+import { useAppDispatch } from "../../store/store";
+import { fetchOrderListAction } from "../../store/slices/orderListSlice";
+
+const { Title } = Typography;
+
 export default function Orders() {
-  return <div>Orders</div>;
+  let [searchParams, setSearchParams] = useSearchParams();
+  const dispatch = useAppDispatch();
+
+  const items: TabsProps["items"] = [
+    {
+      key: "pending",
+      label: "Pending Orders",
+      children: (
+        <>
+          <PaymentOrderTable />
+        </>
+      ),
+    },
+    {
+      key: "complete",
+      label: "Complete Orders",
+      children: (
+        <>
+          <PaymentOrderTable />
+        </>
+      ),
+    },
+  ];
+
+  const onOrderTabChange = useCallback(
+    (key: string) => {
+      setSearchParams({ type: key });
+    },
+    [searchParams]
+  );
+
+  // CALLED API
+  useEffect(() => {
+    const type = searchParams.get("type");
+    if (type) dispatch(fetchOrderListAction(type));
+  }, [searchParams.get("type")]);
+
+  return (
+    <div className={Style.order_container}>
+      <Row gutter={[16, 16]}>
+        <Col span={24}>
+          <Title level={5}>ORDERS </Title>
+        </Col>
+        <Col span={24}>
+          <Tabs
+            defaultActiveKey="1"
+            items={items}
+            onChange={onOrderTabChange}
+            className={Style.tab}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
 }
