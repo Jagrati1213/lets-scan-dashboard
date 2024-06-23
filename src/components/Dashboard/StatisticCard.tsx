d50c2c281de50ad5652ee782717c22d01939d362
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-23 18:40:35 +0530

    JG: dashboard UI

---
+++ src/components/Dashboard/StatisticCard.tsx
@@ -0,0 +1,31 @@
+import { Card, Statistic } from "antd";
+import React from "react";
+
+interface CardPropsT {
+  icon: React.ReactNode;
+  title: string;
+  color: string;
+  value: number;
+  precision: number;
+}
+export function StatisticCard({
+  icon,
+  title,
+  color,
+  value,
+  precision,
+}: CardPropsT) {
+  return (
+    <div>
+      <Card bordered={false}>
+        <Statistic
+          title={title}
+          value={value}
+          precision={precision}
+          valueStyle={{ color: color }}
+          prefix={icon}
+        />
+      </Card>
+    </div>
+  );
+}
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,16 +1,22 @@
 import Title from "antd/es/typography/Title";
 import { useAppDispatch, useAppSelector } from "../../store/store";
-import { Col, Row, Space, Switch, Typography } from "antd";
+import { Card, Col, Row, Space, Switch, Typography } from "antd";
 import { useState } from "react";
 import { updateShopAvailability } from "../../apis/user/updateShopAvailability";
 import { setUserDetailsAction } from "../../store/slices/vendorSlice";
+import { FaBoxOpen } from "react-icons/fa";
+import { StatisticCard } from "./StatisticCard";
+import StatisticsGraph from "./StatisticsGraph";
+import Style from "../../styles/_Dashboard.module.scss";
 
 const { Text } = Typography;
 
 export default function Dashboard() {
   const { vendor } = useAppSelector((store) => store.authSlice);
   const dispatch = useAppDispatch();
-  const [shopIsOpen, setShopIsOpen] = useState(vendor?.isOpen || false);
+  const [shopIsOpen, setShopIsOpen] = useState<boolean>(
+    vendor?.isOpen || false
+  );
 
   // CHANGE SHOP OPENING
   const handleShopAvailability = async (checked: boolean) => {
@@ -24,16 +30,74 @@ export default function Dashboard() {
   };
 
   return (
-    <Row justify={"space-between"} align={"middle"}>
-      <Col>
-        {vendor && <Title level={4}>Welcome, {vendor?.username} </Title>}
-      </Col>
-      <Col>
-        <Space direction="vertical" align="center">
-          <Text>{shopIsOpen ? "CLOSE SHOP" : "OPEN SHOP"}</Text>
-          <Switch value={shopIsOpen} onChange={handleShopAvailability} />
-        </Space>
-      </Col>
-    </Row>
+    <div className={Style.dashboard}>
+      <Row justify={"space-between"} align={"middle"}>
+        <Col>
+          {vendor && <Title level={4}>Welcome, {vendor?.username} </Title>}
+        </Col>
+        <Col>
+          <Space direction="vertical" align="center">
+            <Text>{shopIsOpen ? "CLOSE SHOP" : "OPEN SHOP"}</Text>
+            <Switch value={shopIsOpen} onChange={handleShopAvailability} />
+          </Space>
+        </Col>
+      </Row>
+
+      <Row justify={"start"} align={"middle"} gutter={[16, 20]}>
+        <Col span={24}>
+          <Row gutter={[16, 20]} justify={"center"}>
+            <Col span={24} sm={12} md={6}>
+              <StatisticCard
+                title={"TOTAL ORDERS"}
+                value={44}
+                precision={0}
+                color="#cf1322"
+                icon={<FaBoxOpen />}
+              />
+            </Col>
+            <Col span={24} sm={12} md={6}>
+              <StatisticCard
+                title={"TOTAL ORDERS"}
+                value={44}
+                precision={0}
+                color="#cf1322"
+                icon={<FaBoxOpen />}
+              />
+            </Col>
+            <Col span={24} sm={12} md={6}>
+              <StatisticCard
+                title={"TOTAL ORDERS"}
+                value={44}
+                precision={0}
+                color="#cf1322"
+                icon={<FaBoxOpen />}
+              />
+            </Col>
+            <Col span={24} sm={12} md={6}>
+              <StatisticCard
+                title={"TOTAL ORDERS"}
+                value={44}
+                precision={0}
+                color="#cf1322"
+                icon={<FaBoxOpen />}
+              />
+            </Col>
+          </Row>
+        </Col>
+
+        <Col span={24}>
+          <Card title="STATUS">
+            <Row gutter={[16, 30]}>
+              <Col>
+                <StatisticsGraph />
+              </Col>
+              <Col>
+                <StatisticsGraph />
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 }
