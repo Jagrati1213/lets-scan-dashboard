a3b4b20dff6916f11a4feb75710d4147aa982016
Jagrati Gupta <85276293+Jagrati1213@users.noreply.github.com>
2025-08-27 14:54:23 +0530

    Update global.ts

--- src/global.ts
+++ src/global.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 // AXIOS BASE URL
 export const Axios = axios.create({
   // baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL:"https://lets-scan-server.vercel.app/"
+  // baseURL:"https://lets-scan-server.vercel.app/"
+  baseUrl/:"https://lets-scan-server.vercel.app/"
 });
 
 // AXIOS INTERCEPTOR INCLUDE ACCESS & REFRESH TOKEN
